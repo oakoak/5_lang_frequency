7e23f2e3b5a56379b7c89a8e8c58ce16a957ee17
varan <varan@VARAN-PC>
2018-07-13 00:12:01 +0300

    written script and readme

--- README.md
+++ README.md
@@ -1,6 +1,43 @@
 # Frequency Analysis of Words
+---
+## Module **lang_frequency.py** returns the most common words in text
+---
 
-[TODO. There will be project description]
+### Discription
+
+### Script usage example
+```python
+import lang_frequency
+text = lang_frequency.load_data("you_file.txt")
+lang_frequency.get_most_frequent_words(text)
+```
+
+### How starting
+```bash
+$ python lang_frequency.py
+```
+
+### Example result
+```bash
+и
+не
+в
+что
+на
+а
+но
+я
+как
+с
+```
+
+##  Where to get data:
+You can take any text file
+
+### Requirements
+```bash
+Python ver 3.5 (or higher)
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,31 @@
+import string
+import collections
+import operator
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, "r") as file:
+        return file.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    words = filter(None, [word.strip(string.punctuation).lower()
+                          for word in text.split()])
+    frequency_words = collections.Counter(words)
+    sorted_words = sorted(frequency_words.items(),
+                          key=operator.itemgetter(1), reverse=True)
+    for i in sorted_words[:10]:
+        print(i[0])
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        file_path = sys.argv[1]
+        text = load_data(file_path)
+    except IndexError:
+        exit("Error: No filename for reading!")
+    except FileNotFoundError:
+        exit("Error: file or path '{0}' not found!\n".format(file_path))
+
+    get_most_frequent_words(text)
